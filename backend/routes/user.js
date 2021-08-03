a603a6e4783a9c2614c35c8022db21132c747bdb
KuroiTengokuWeb <78926523+KuroiTengokuWeb@users.noreply.github.com>
2021-08-03 13:10:40 +0200

    Add helmet, crypto-js + update sécu et com

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,10 @@
+// Importations
 const express = require('express');
 const router = express.Router();
-
 const userCtrl = require('../controllers/user');
-
 const rateLimitLogin = require('../middleware/rate-limit-login-config');
 
+// Routes
 router.post('/signup', userCtrl.signup);
 router.post('/login', rateLimitLogin, userCtrl.login);
 
